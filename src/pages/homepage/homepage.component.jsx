449ebef700dd4b61cbb31c2f8a12463d58bc3853
Rahul <rahulchandlawala@gmail.com>
2022-04-08 16:24:43 +0530

    Updated the menubar to individial compoents

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../componets/directory/directory.compoent.jsx";
+import MenuItem from "../../componets/MenuItem/menu-item.componet.jsx";
+import "./homepage.style.scss";
+
+const Homepage = () => (
+	<div className="homepage">
+		<Directory />
+	</div>
+);
+
+export default Homepage;
